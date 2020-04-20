3373f1f6c9543d6fd478dd81b53e5d50b5d63070
suriyaa <suriyaakvsoff@gmail.com>
2020-04-20 12:21:47 +0530

    Updated Login and Signup;

--- controller/fetchController.js
+++ controller/fetchController.js
@@ -1,10 +1,75 @@
 
 const { con } = require('../utils/db')
 const stringify = require("csv-stringify");
+var bcrypt = require('bcryptjs');
+
+
+checkForExistingUser = (email) => {
+    return new Promise(function (resolve, reject) {
+        con.query(`select * from user where email='${email}'`, (err, result) => {
+            if (err) throw err;
+            resolve(result)
+        })
+    })
+}
+
+generateHash = (password) => {
+    return new Promise(function (resolve, reject) {
+        bcrypt.genSalt(10, function (err, salt) {
+            bcrypt.hash(password, salt, function (err, hash) {
+                resolve(hash)
+            });
+        });
+    })
+}
+
+exports.registerUser = async (request, response) => {
+    let { userName, email, password } = request.body;
+    let isExisting = await checkForExistingUser(email)
+    console.log("Existing User", isExisting)
+    if (isExisting.length == 0) {
+        let hashPassword = await generateHash(password)
+        con.query(`Insert into user (name,email,password) values('${userName}','${email}','${hashPassword}')`, (err, result) => {
+            if (err) throw err;
+            response.json({
+                data: "Inserted Successfully in to the database"
+            })
+        })
+    }
+    else {
+        response.json({
+            data: "Email Already Exists"
+        })
+    }
+}
+
+exports.validateUser = async (request, response) => {
+    const { email, password } = request.body;
+    const isExisting = await checkForExistingUser(email)
+    if (isExisting.length == 0) {
+        response.status(404).json({
+            data: "Email doesn't Existss"
+        })
+    } else {
+        bcrypt.compare(password, isExisting[0].password).then((res) => {
+            if (res) {
+                response.status(200).json({
+                    info: "Login Successful",
+                    email:isExisting[0].email,
+                    userName:isExisting[0].name
+                })
+            } else {
+                response.status(404).json({
+                    data: "Invalid Credentials"
+                })
+            }
+        });
+    }
+}
 
 exports.fetchRecordsWithMysql = function (req, res) {
     const { pageNum, NoOfRecords } = req.body
-    
+
     var skip = (pageNum - 1) * NoOfRecords;
     var limit = skip + ',' + NoOfRecords;
 
@@ -30,6 +95,6 @@ exports.downloadCSV = function (req, res) {
 exports.getTotal = function (req, res) {
     con.query(`select count(*) as TotalCount from porto`, (err, count) => {
         if (err) throw err;
-        res.json({count})
+        res.json({ count })
     })
 }
--- routes/fetchRoutes.js
+++ routes/fetchRoutes.js
@@ -6,13 +6,19 @@ router.post('/fetchRecordsWithMssql', function(req, res) {
   fetchController.fetchRecordsWithMysql(req,res)
 });
 
+router.post('/registerUser',function(request,response){
+  fetchController.registerUser(request,response)
+})
+
+router.post('/validateUser',function(request,response){
+  fetchController.validateUser(request,response)
+})
+
 router.get('/downloadCSV', function(req, res) {
   fetchController.downloadCSV(req,res)
 });
 router.get('/getTotal', function(req, res) {
   fetchController.getTotal(req,res)
 });
-
-
 
 module.exports = router;
